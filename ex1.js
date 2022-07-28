92e3ed4125be7ead220e46f16e68853ace32d379
danilordev <109250477+danilordev@users.noreply.github.com>
2022-07-28 15:35:44 -0300

    npm

--- ex1.js
+++ ex1.js
@@ -1,6 +1,6 @@
 // Crie uma função que irá imprimir a palavra "ATENTA"
 
-dddd
+
 
 imprimeAtenta(); // Aqui estamos instânciando, chamando a função que você construiu
 // ATENTA 
--- ex2.js
+++ ex2.js
@@ -14,3 +14,4 @@ const readline = require('readline').createInterface({
     exibeIdade(ano);
     readline.close()
   })
+  
